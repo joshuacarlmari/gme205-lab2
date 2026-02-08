f3b5bdf2b3f61c932beb3425235a99650ede4245
joshuacarlmari <joshuacarlmarifa@gmail.com>
2026-02-08 12:08:09 +0800

    Added Identity and Coordinates

---
+++ src/demo.py
@@ -0,0 +1,4 @@
+from spatial import Point
+
+p = Point("A", 121.0, 14.6)
+print(p.id, p.lon, p.lat)
---
+++ src/spatial.py
@@ -0,0 +1,5 @@
+class Point:
+    def __init__(self, id, lon, lat):
+        self.id = id
+        self.lon = lon
+        self.lat = lat
