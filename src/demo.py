e6e4e797d8c315f0742e185e4a8a882bb64de6af
joshuacarlmari <joshuacarlmarifa@gmail.com>
2026-02-08 12:41:54 +0800

    Added static method for spatial math

--- src/demo.py
+++ src/demo.py
@@ -6,3 +6,7 @@ print(p.to_tuple())
 
 q = Point("X", 150, 14)
 print(q.id, q.lon, q.lat)
+
+distance = p.distance_to(q)
+
+print(f"p → q: {distance:.2f} m")
--- src/spatial.py
+++ src/spatial.py
@@ -1,3 +1,4 @@
+import math
 class Point:
     def __init__(self, id, lon, lat, name=None, tag=None):
         if not (-180 <= lon <= 180):
@@ -19,3 +20,33 @@ class Point:
         Return the coordinate as a (lon, lat) tuple.
         """
         return (self.lon, self.lat)
+    
+    def distance_to(self,other):
+        return Point.haversine_m(self.lon, self.lat, other.lon, other.lat)
+    
+# ------------------------------------------------------------------
+    # Static method (pure spatial math)
+    # ------------------------------------------------------------------
+
+    @staticmethod
+    def haversine_m(
+        lon1: float, lat1:float, lon2:float, lat2:float
+       )   -> float:
+        """
+        Compute the Haversine Distance between tow lon/lat pairs in meters.
+
+        Static method because it does not depend on object state.
+        """
+
+        R = 6_371_000.0 # Earth radius in meters
+
+        phi1 = math.radians(lat1)
+        phi2 = math.radians(lat2)
+        dphi = math.radians(lat2 - lat1)
+        dlambda = math.radians(lon2 - lon1)
+
+        a = math.sin(dphi/2)**2 + math.cos(phi1) * math.cos(phi2) * math.sin(dlambda/2)**2
+        c = 2 * math.asin(math.sqrt(a))
+
+        distance = R * c
+        return distance
