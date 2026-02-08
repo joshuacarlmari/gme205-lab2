711d6babf5fdf50b4aea146b98a2851ac8635255
joshuacarlmari <joshuacarlmarifa@gmail.com>
2026-02-08 12:15:39 +0800

    Enforced Spatial Validity

--- src/demo.py
+++ src/demo.py
@@ -2,3 +2,6 @@ from spatial import Point
 
 p = Point("A", 121.0, 14.6)
 print(p.id, p.lon, p.lat)
+
+q = Point("X", 999, 14)
+print(q.id, q.lon, q.lat)
--- src/spatial.py
+++ src/spatial.py
@@ -1,5 +1,11 @@
 class Point:
     def __init__(self, id, lon, lat):
+        if not (-180 <= lon <= 180):
+            raise ValueError("Longitude must be between -180 and 180")
+        
+        if not (-90 <= lat <= 90):
+            raise ValueError("Latitude must be between -90 and 90")
+
         self.id = id
         self.lon = lon
         self.lat = lat
